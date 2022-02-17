12f48c3d932a307dc10505d87a5574d959bec17a
GitHub <noreply@github.com>
2022-02-17 00:50:46 +0000

    chore(deps): update deno dependencies

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 import {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.123.0/testing/asserts.ts";
+} from "https://deno.land/std@0.126.0/testing/asserts.ts";
 import { sanitize, tag } from "./mod.ts";
 
 Deno.test("render tag", () => {
