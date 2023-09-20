94cc6e61f290d0b1a43215d9c0ee0f736b22173b
GitHub <noreply@github.com>
2023-09-20 00:57:27 +0000

    chore(deps): update deno dependencies

--- test.ts
+++ test.ts
@@ -1,7 +1,7 @@
 import {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.201.0/testing/asserts.ts";
+} from "https://deno.land/std@0.202.0/testing/asserts.ts";
 import { generateTag, sanitize, tag, tagNoVoid, tagVoid } from "./mod.ts";
 
 Deno.test("render tag", () => {
