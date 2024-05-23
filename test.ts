232368ad099117bef2143dd60dc742609fdd0195
kawarimidoll <kawarimidoll+git@gmail.com>
2024-05-24 07:46:32 +1000

    chore: use jsr instead of deno.land/std

--- test.ts
+++ test.ts
@@ -1,7 +1,4 @@
-import {
-  assertEquals,
-  assertThrows,
-} from "https://deno.land/std@0.224.0/testing/asserts.ts";
+import { assertEquals, assertThrows } from "@std/assert";
 import { generateTag, sanitize, tag, tagNoVoid, tagVoid } from "./mod.ts";
 
 Deno.test("render tag", () => {
